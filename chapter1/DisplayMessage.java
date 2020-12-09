740fef2e243453b6167d67ebacc7de55a6815a0b
Semiu <ayobami.sm@gmail.com>
2020-12-09 03:13:02 -0600

    chapter 1 started

---
+++ chapter1/DisplayMessage.java
@@ -0,0 +1,15 @@
+//Chapter 1, programming exercise 1.2
+public class DisplayMessage {
+
+	private static int numOfTimes = 5;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 1; i<=numOfTimes; i++) {
+			
+			System.out.println("I love Java!");
+			
+		}
+	}
+
+}
