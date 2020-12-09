0a566ec5ae865ed321e705988c9b9f22111e61c9
Semiu <ayobami.sm@gmail.com>
2020-12-09 03:23:02 -0600

    chapter 1 exercises commented

--- chapter1/DisplayMessage.java
+++ chapter1/DisplayMessage.java
@@ -1,6 +1,8 @@
 //Chapter 1, programming exercise 1.2
+//Display "I love Java!" in a number of times
 public class DisplayMessage {
-
+	// The number of times the message is to be displayed
+	//Declared as static because it is used in loop and thus not meant for any change
 	private static int numOfTimes = 5;
 	
 	public static void main(String[] args) {
--- chapter1/Welcome.java
+++ chapter1/Welcome.java
@@ -1,3 +1,5 @@
+//Chapter 1, programming exercise 1.1 
+// Display "I love Java!" in the console
 public class Welcome {
 
 	public static void main(String[] args) {
