9e728ad7b1811cf2146021a7496f5b479580965d
Semiu <ayobami.sm@gmail.com>
2021-01-23 22:39:38 -0600

    Extra implementation

---
+++ Intro-to-Java/src/com/threads/MainThread.java
@@ -0,0 +1,36 @@
+package com.threads;
+
+public class MainThread {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello from the main thread!");
+		
+		//Object of the OneThread class
+		Thread oneThread = new OneThread();
+		
+	//Invocking the start method of the Thread class on the Thread subclass to start the thread
+		oneThread.start();
+		
+		new Thread () {
+			public void run() {
+				System.out.println("Hello from the anonymous thread!");
+			}
+		}.start();
+		
+		Thread myRunnable = new Thread(new MyRunnable() {
+			@Override
+			public void run() {
+				//super.run();
+				System.out.println("A thread from anonymous run");
+			}
+		});
+		
+		myRunnable.start();
+		
+		System.out.println("Hello again from the main thread!");
+		
+		//oneThread.start(); Running this would create an IllegalThreadException because one thread is allowed
+	}
+
+}
---
+++ Intro-to-Java/src/com/threads/MyRunnable.java
@@ -0,0 +1,12 @@
+package com.threads;
+
+public class MyRunnable implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println("A thread from my runnable");
+	}
+	
+
+}
