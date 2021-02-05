d6c43fcab29ec41765616ba6033922d2e663b580
Semiu <ayobami.sm@gmail.com>
2021-02-04 21:35:57 -0600

    proxy server assigment started

---
+++ ProxyServer/src/com/DSClass/ProxyServer.java
@@ -0,0 +1,128 @@
+package com.DSClass;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ProxyServer {
+
+	/**
+	 * Declaring the class instance variables
+	 */
+	//cache is a Map: the key is the URL and the value is the file name of the file that stores the cached content
+	Map<String, String> cache;
+	
+	//socket variable for the server
+	ServerSocket proxySocket;
+	
+	//The file name of the file to store the cached content
+	String logFileName = "log.txt";
+	
+	//Declare a running boolean variable to track when client-server connection is successful
+	boolean running;
+
+	//The main method for the program
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//I assume that this startServer should take port number as argument (Perhaps this would come from the browwer)
+		//new ProxyServer().startServer(Integer.parseInt(args[0]));
+		new ProxyServer().startServer(8080); //testing
+
+	}
+	
+	//startServer method of the ProxyServer class 
+	void startServer(int proxyPort) {
+		
+
+		cache = new ConcurrentHashMap<>();
+
+		// create the directory to store cached files. 
+		File cacheDir = new File("cached");
+		if (!cacheDir.exists() || (cacheDir.exists() && !cacheDir.isDirectory())) {
+			//checking if file exists in Cache directory
+			System.out.println("File not found in cached directory - creating new directory");
+			cacheDir.mkdirs();
+		}	
+				
+		// Create the Server Socket for the Proxy 
+		try {
+			
+			proxySocket = new ServerSocket(proxyPort);
+			
+			System.out.println("Waiting for client on port " + proxySocket.getLocalPort() + "..");
+			running = true;
+		} 
+
+		// Catch exceptions associated with opening socket
+		catch (SocketException se) {
+			System.out.println("Socket Exception when connecting to client");
+			se.printStackTrace();
+		}
+		catch (IOException io) {
+			System.out.println("IO exception when connecting to client");
+		}
+		
+		//When connection is successful 
+		/**
+		 * A serverSocket to listen on the port (proxyPort) and accept new socket connections
+		 *  (Remember to catch Exceptions!)
+		 */
+		while(running){
+			try {
+				// proxySocket.accept() Blocks until a connection is made
+				Socket socket = proxySocket.accept();
+				
+				/**
+				 * A thread (RequestHandler) for each new client connection 
+				 */
+				Thread thread = new Thread(new RequestHandler(socket));
+				
+				// Key a reference to each thread so they can be joined later if necessary
+				//This is if there is a need to take track of the threads. ArrayList of operating threads would be created
+				//operatingThreads.add(thread);
+				
+				//Start the thread
+				thread.start();	
+				
+			} catch (SocketException e) {
+				
+				// Socket exception is triggered by management system to shut down the proxy 
+				System.out.println("Server closed");
+				
+			} catch (IOException e) {
+				
+				e.printStackTrace();
+			}
+		}
+		
+		
+	}
+	//end of startServer method
+	
+	//Method to get the cache's hashcode
+	public String getCache(String hashcode) {
+		return cache.get(hashcode);
+	}
+
+	//Method to save into the cache Map.
+	public void putCache(String hashcode, String fileName) {
+		cache.put(hashcode, fileName);
+	}
+
+	//Method to write info into the log file.
+	//
+	public synchronized void writeLog(String info) {
+		
+			/**
+			 * To do: write string (info) to the log file, and add the current time stamp 
+			 * e.g. String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+			 *
+			*/
+	}
+
+}
---
+++ ProxyServer/src/com/DSClass/RequestHandler.java
@@ -0,0 +1,148 @@
+package com.DSClass;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.SecureRandom;
+
+//
+public class RequestHandler extends Thread {
+	
+	//Socket connected to client passed by Proxy server
+	Socket clientSocket;
+
+	InputStream inFromClient;
+
+	OutputStream outToClient;
+	
+	byte[] request = new byte[1024];
+
+	BufferedReader proxyToClientBufferedReader;
+
+	BufferedWriter proxyToClientBufferedWriter;
+
+	//changes here to the template provided---
+	//ProxyServer proxyServer;
+
+	//We only use the clientSocket, why passing the proxyServer as an argument
+	public RequestHandler(Socket clientSocket) {
+
+		
+		this.clientSocket = clientSocket;
+		
+
+		//this.proxyServer = proxyServer;
+
+		try {
+			clientSocket.setSoTimeout(2000);
+			inFromClient = clientSocket.getInputStream();
+			outToClient = clientSocket.getOutputStream();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	
+	@Override
+	public void run() {
+
+		/**
+			 * To do: Process the requests from a client. In particular
+		*/
+		
+		/**
+		 * (1) Check the request type, only process GET request and ignore others
+		 */
+		
+		/**
+		 * (2) If the url of GET request has been cached, respond with cached content
+		 */
+		
+		/**
+		 * (3) Otherwise, call method proxyServertoClient to process the GET request
+		 */
+
+	}
+
+	
+	private boolean proxyServertoClient(byte[] clientRequest) {
+
+		FileOutputStream fileWriter = null;
+		Socket serverSocket = null;
+		InputStream inFromServer;
+		OutputStream outToServer;
+		
+		// Create Buffered output stream to write to cached copy of file
+		//String fileName = "cached/" + generateFileName() + ".dat";
+		
+		// to handle binary content, byte is used
+		byte[] serverReply = new byte[4096];
+		
+			
+		/**
+		 * To do
+		 * (1) Create a socket to connect to the web server (default port 80)
+		 * (2) Send client's request (clientRequest) to the web server, you may want to use flush() after writing.
+		 * (3) Use a while loop to read all responses from web server and send back to client
+		 * (4) Write the web server's response to a cache file, put the request URL and cache file name to the cache Map
+		 * (5) close file, and sockets.
+		*/
+	
+	}
+	
+	
+	
+	// Sends the cached content stored in the cache file to the client
+	private void sendCachedInfoToClient(String fileName) {
+
+		try {
+
+			byte[] bytes = Files.readAllBytes(Paths.get(fileName));
+
+			outToClient.write(bytes);
+			outToClient.flush();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+
+			if (clientSocket != null) {
+				clientSocket.close();
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		}
+	}
+	
+	
+	// Generates a random file name  
+	public String generateRandomFileName() {
+
+		String ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_";
+		SecureRandom RANDOM = new SecureRandom();
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < 10; ++i) {
+			sb.append(ALPHABET.charAt(RANDOM.nextInt(ALPHABET.length())));
+		}
+		return sb.toString();
+	}
+	
+	
+	
+	
+	
+	
+
+}
